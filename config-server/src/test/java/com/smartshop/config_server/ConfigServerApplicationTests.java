de641909fadbadc8801f57d65487e9c4964b669b
RupeshPant16 <pantrupesh05@gmail.com>
2025-11-08 20:32:05 +0530

    Initial setup

---
+++ config-server/src/test/java/com/smartshop/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartshop.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
